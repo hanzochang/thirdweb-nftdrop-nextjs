11c0b7e3f7a0aac99c096419d021f0f41f0e0145
hanzochang <hanzo.chan@gmail.com>
2022-09-15 18:07:07 +0900

    Change testnet and contract address

--- components/templates/Minting.tsx
+++ components/templates/Minting.tsx
@@ -45,7 +45,7 @@ const Component: React.FC = () => {
               {store.claimedSupply} / {store.totalSupply}
             </Text>
             <Text pt={2} fontSize="xs" textAlign={'center'}>
-              rinkeby testnet
+              goerli testnet
             </Text>
           </div>
         </VStack>
